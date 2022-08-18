42f480eb8d7c55d9d0703421dfb240a8ccbed94b
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-18 19:53:49 +0000

    fixed typo

--- scripts/lotteryScript.ts
+++ scripts/lotteryScript.ts
@@ -1,14 +1,17 @@
 import { ethers } from "hardhat";
 
 const main = async () => {
+  const Lottery = await ethers.getContractAt(
+    "Ilottery",
+    "0x4385B18589eaFcA91A5E396baFA98db0e8a4aEA7"
+  );
 
-    const Lottery = await ethers.getContractAt("Ilottery", "0x4385B18589eaFcA91A5E396baFA98db0e8a4aEA7");
-
-    const joinLottery = await Lottery.join({value: ethers.utils.parseEther("1")});
-    const receipt = await joinLottery.wait();
-    console.log("Receipt hash: ", receipt);
-
-  
+  const joinLottery = await Lottery.join({
+    value: ethers.utils.parseEther("1"),
+  });
+  const receipt = await joinLottery.wait();
+  console.log("Receipt hash: ", receipt);
+};
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
